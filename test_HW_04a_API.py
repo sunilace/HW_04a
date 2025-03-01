02a6c214b3ad7ff80a804d3966a8a1cfef786482
Sunil Gupta <48219206+sunilace@users.noreply.github.com>
2025-02-28 20:40:27 -0500

    Update to implement mock

--- test_HW_04a_API.py
+++ test_HW_04a_API.py
@@ -1,11 +1,25 @@
 from HW_04a_API import details
+from unittest.mock import Mock, patch
 
 
-def test_status_code_404_invalid_id():
+@patch(HW_04a_API.details)
+def test_status_code_404_invalid_id(mock_get):
+    mock_get.return_value = "Status Code: 404"
     assert details("Invalid_Github_Id") == "Status Code: 404"
 
 
-def test_working_API_Example():
+@patch(HW_04a_API.details)
+def test_working_API_Example(mock_get):
+    mock_get.return_value = """Repository name: csp, Commits: 2
+Repository name: hellogitworld, Commits: 50
+Repository name: helloworld, Commits: 6
+Repository name: Mocks, Commits: 10
+Repository name: Project1, Commits: 2
+Repository name: richkempinski.github.io, Commits: 9
+Repository name: threads-of-life, Commits: 1
+Repository name: try_nbdev, Commits: 2
+Repository name: try_nbdev2, Commits: 5
+"""
     data = """Repository name: csp, Commits: 2
 Repository name: hellogitworld, Commits: 50
 Repository name: helloworld, Commits: 6
